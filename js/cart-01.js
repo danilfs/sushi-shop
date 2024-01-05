2c02785a65dd4fcd9a43d0e4a9a3f32bd0cb1d50
dsfrantsev <fds@avito.ru>
2024-01-05 16:00:13 +0300

    add product

--- js/cart-01.js
+++ js/cart-01.js
@@ -1,3 +1,5 @@
+const cartWrapper = document.querySelector(".cart-wrapper");
+
 // отслеживаем клик по странице
 window.addEventListener("click", (event) => {
   //проверяем что был клик по Добавить в корзину
@@ -10,7 +12,43 @@ window.addEventListener("click", (event) => {
 
     const productInfo = {
       id: card.dataset.id,
-      imgSrc: card.querySelector(".product-img").getAttribute(".src"),
+      imgSrc: card.querySelector(".product-img").getAttribute("src"),
+      title: card.querySelector(".item-title").innerText,
+      itemsInBox: card.querySelector("[data-items-in-box]").innerText,
+      weight: card.querySelector(".price__weight").innerText,
+      price: card.querySelector(".price__currency").innerText,
+      counter: card.querySelector("[data-counter]").innerText,
     };
+    console.log(productInfo);
+
+    const cardItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+    <div class="cart-item__top">
+        <div class="cart-item__img"
+            <img src="${productInfo.imgSrc}" alt=${productInfo.title}>
+        </div>
+        <div class="cart-item__desc">
+            <div class="cart-item__title">${productInfo.title}</div>
+            <div class="cart-item__weight">6 шт. / 205г.</div>
+
+            <!-- cart-item__details -->
+            <div class="cart-item__details">
+
+                <div class="items items--small counter-wrapper">
+                    <div class="items__control" data-action="minus">-</div>
+                    <div class="items__current" data-counter="">1</div>
+                    <div class="items__control" data-action="plus">+</div>
+                </div>
+
+                <div class="price">
+                    <div class="price__currency">${productInfo.price}</div>
+                </div>
+
+            </div>
+            <!-- // cart-item__details -->
+
+        </div>
+    </div>
+</div>`;
+    cartWrapper.insertAdjacentHTML("beforeend", cardItemHTML);
   }
 });

e354283b1814c7df9b885921b2c2c6db8b7d17fe
dsfrantsev <fds@avito.ru>
2024-01-07 23:30:20 +0300

    calculate price and count

---
+++ js/calcCartPrice.js
@@ -0,0 +1,16 @@
+function calcCardPrice() {
+  const cartItems = document.querySelectorAll(".cart-item");
+  let totalPrice = 0;
+
+  cartItems.forEach(function (item) {
+    const amountEl = item.querySelector("[data-counter]");
+    const priceEl = item.querySelector(".price__currency");
+
+    const currentPrice =
+      parseInt(amountEl.innerText) * parseInt(priceEl.innerText);
+
+    totalPrice += currentPrice;
+  });
+
+  console.log(totalPrice);
+}
